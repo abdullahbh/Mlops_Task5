d77260d516ea9bbb1d53564504b0b165b656344d
Abdullah <themacshop@abdullahmac.local>
2024-03-28 08:57:49 +0500

    first commit

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://mongodb:27017/myDatabase"  # Docker service name for MongoDB
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/mlops"  # Docker service name for MongoDB
 mongo = PyMongo(app)
 
 @app.route('/submit', methods=['POST'])
